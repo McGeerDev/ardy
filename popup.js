33c9c35e7bfef48f4a11616306c5c799148185b3
dmcgeer <devan@rezco.co.za>
2024-02-01 17:00:23 +0200

    Removed unnecessary logs and alerts

--- popup.js
+++ popup.js
@@ -1,8 +1,6 @@
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   chrome.tabs.sendMessage(tabs[0].id, { action: "getVideoInfo" }, function (response) {
     document.getElementById("info").innerText = `Adjusted Length: ${response.adjustedLength} seconds`;
-    alert(`Adjusted Length: ${response.adjustedLength} seconds`);
-    console.log(`Adjusted Length: ${response.adjustedLength} seconds`);
   });
 });
 
