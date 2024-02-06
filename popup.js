0b1e99848c2a97b93227eeada29e905ad1eeb5ed
dmcgeer <devan@rezco.co.za>
2024-02-06 09:39:01 +0200

    Using chrome scripting instead of tabs for manifest v3

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
-chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  chrome.tabs.sendMessage(tabs[0].id, { action: "getVideoInfo" }, function (response) {
+chrome.scripting.query({ active: true, currentWindow: true }, function (tabs) {
+  chrome.scripting.sendMessage(tabs[0].id, { action: "getVideoInfo" }, function (response) {
     document.getElementById("info").innerText = `Adjusted Length: ${response.adjustedLength} seconds`;
   });
 });
