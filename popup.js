66b8a4944b91746dd809788f755a91b82ca90047
dmcgeer <devan@rezco.co.za>
2024-02-07 11:32:40 +0200

    Removed poput files

--- popup.js
+++
@@ -1,6 +0,0 @@
-chrome.scripting.query({ active: true, currentWindow: true }, function (tabs) {
-  chrome.scripting.sendMessage(tabs[0].id, { action: "getVideoInfo" }, function (response) {
-    document.getElementById("info").innerText = `Adjusted Length: ${response.adjustedLength} seconds`;
-  });
-});
-
